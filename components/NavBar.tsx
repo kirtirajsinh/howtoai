0b7f8f958567c1ab139d7fc63d19cff2ee204ec5
kirtirajsinh <kirtiraj3738@gmail.com>
2024-08-29 10:38:35 +0530

    chore: Update max width in NavBar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,12 +3,12 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
+    <nav className="w-full relative flex items-center justify-between max-w-6xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
-        How TO{" "}
         <span className="text-primary  font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
           AI{" "}
         </span>
+        Simplified
       </Link>
 
       <ModeToggle />
